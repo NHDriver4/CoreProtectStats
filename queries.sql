59a6bf476dc3678fa364430c789c51e112166e15
BuildTools <unconfigured@null.spigotmc.org>
2018-11-30 23:55:44 -0500

    Init

--- queries.sql
+++ queries.sql
@@ -1,11 +1,17 @@
-# SELECT Broken Blocks for the last 24 hours
+/* Count Broken Blocks for the last 24 hours */
 SELECT COUNT(*) FROM co_block 
 WHERE co_block.time > UNIX_TIMESTAMP(NOW() - INTERVAL 24 HOUR) AND ACTION = 0
 ORDER BY rowid Desc
 
-# SELECT Broken Blocks for the last week
+/* Count Broken Blocks for the last week */
 SELECT COUNT(*) FROM co_block 
 WHERE co_block.time > UNIX_TIMESTAMP(NOW() - INTERVAL 1 WEEK) AND ACTION = 0
 ORDER BY rowid Desc
 
-# Coal - 13
+/* Count Broken Iron */
+
+/* Count Broken Gold Ore */
+
+/* Count Broken Diamond Ore */
+
+/* Count Broken Emerald ore */
